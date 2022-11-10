1a84aa93b2496d8157655a4180a6bda6f57fdfc5
JoseSalado <saladojosemiguel@gmail.com>
2022-11-10 19:31:55 -0300

    agregue ultimos estilos

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../NavBar'
 import { Outlet } from 'react-router-dom'
+import './styles.css'
 
 const Root = () => {
   return (
     <>
         <NavBar/>
+        <h2 className='title-item-container'>Rick y Morty NFT`s</h2>
         <Outlet/>
        
     </>
